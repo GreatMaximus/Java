2632d858eac1d676809e10ece1788cb1cb0e3846
GraetMaximus <ablgl666@gmail.com>
2023-02-13 20:04:10 +0200

    first lesson

---
+++ Java/src/com/company/Main2.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main2 {
+    public static void main(String[] args){
+        int num = 10;
+        float num2 = 10.5f;
+        double num3 = 20.8d;
+        byte num4 = 125;// byte [-128, 128)
+        short num5 = 32000;
+        long num6 = 568544689;
+        String str = "Hello";
+        char sym = '*';
+        boolean isTrue = true; // true or false
+    }
+}
