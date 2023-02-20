d8186ac83b6538acbb7529d0c42309a1f47f2865
GraetMaximus <ablgl666@gmail.com>
2023-02-20 17:55:24 +0200

    for

---
+++ Java/src/com/company/Main8.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main8 {
+    public static void main (String[] args){
+        for (int i = 10;i>=-10;i--){
+
+            if (i == 0) {
+                System.out.println (i + " Water is frozen" );
+            }
+            else {
+                System.out.println (i);
+            }
+        }
+    }
+}
