1888bbf13c06f0e10a3a96c70803f1166889e016
GreatMaximus <ablgl666@gmail.com>
2023-02-28 12:43:06 +0200

    homework

--- Java/src/com/company/Main13.java
+++ Java/src/com/company/Main13.java
@@ -4,7 +4,8 @@ public class Main13 {
     public static void main (String [] args){
         int[] array = {1,2,3,4,5,6,7,8,9,10};
         for (int i = 0; i<array.length; i++){
-            System.out.println(array[i]);
+
+            System.out.println (array[i]);
         }
 
     }
