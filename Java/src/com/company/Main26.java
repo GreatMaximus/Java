9f3d5db3abe52190b71011ccc21d6030cd31cedf
GraetMaximus <ablgl666@gmail.com>
2023-03-03 20:15:53 +0200

    ArrayList, Matrix, total letters words in strings

---
+++ Java/src/com/company/Main26.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main26 {
+    public static void main(String[] args) {
+         int [][] matrixA = new int[][] {{1,2}, {3,4}};
+         int [][] matrixB = new int[][] {{5,6},{7,8}};
+         int [][] matrixC = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+         int [][] matrixD = new int[][] {{10,11,12},{13,14,15},{16,17,18}};
+
+         for(int i=0; i< 2;i++){
+             int result1 = matrixA[i][0]*matrixB[0][0] + matrixA[i][1]*matrixB[1][0];
+             int result2 = matrixA[i][0]*matrixB[0][1] + matrixA[i][1]*matrixB[1][1];
+             System.out.println(result1 + " " + result2);
+
+         }
+        System.out.println();
+
+         for(int i = 0; i<3;i++){
+             int result3 = matrixC[i][0]*matrixD[0][0]+matrixC[i][1]*matrixD[1][0]+matrixC[i][2]*matrixD[2][0];
+             System.out.println(result3 + " ");
+         }
+    }
+}
