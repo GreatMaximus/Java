4c52e1409307170058e9545f68839c2142b0941e
GraetMaximus <ablgl666@gmail.com>
2023-03-07 17:49:44 +0200

    0703

---
+++ Java/src/com/company/Homework1.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        int [] array = {678, 345, 1, 456, 56, -7};
+        int max = array[0];
+        int min = array[0];
+        for (int i = 0; i < array.length; i++){
+            if(max<array[i]) {
+                max = array[i];
+            }
+            if(array[i]<min){
+                min = array[i];
+            }
+        }
+        System.out.println("Biggest number is " + max);
+        System.out.println("Lowest number is " + min);
+    }
+}
