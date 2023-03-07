905801b1e2d9eea200dcea75b0a1713a56c1d71a
GreatMaximus <ablgl666@gmail.com>
2023-03-07 17:38:57 +0200

    HomeMatrix

--- Java/src/com/company/Main13.java
+++ Java/src/com/company/Main13.java
@@ -4,15 +4,15 @@ public class Main13 {
     public static void main (String [] args){
         int sum = 0;
         int[] array = {1,2,3,4,5,6,7,8,9,10};
-<<<<<<< HEAD
+
         for (int i = 0; i<array.length; i++){
 
             System.out.println (array[i]);
         }
-=======
+
         for (int i = 0; i< array.length; i++){
             sum = sum+ array[i];
->>>>>>> 31c6a64353264ed6bc2a602c7620e1a885f4f453
+
 
         }
         System.out.println(sum);
