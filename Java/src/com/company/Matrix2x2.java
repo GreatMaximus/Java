b0673b213fd8dc937db482c8a6573f31b07b0348
GreatMaximus <ablgl666@gmail.com>
2023-03-09 22:13:31 +0200

    I've tried

---
+++ Java/src/com/company/Matrix2x2.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Matrix2x2 {
+    public static void main (String args){
+        Scanner scn = new Scanner (System.in);
+        System.out.println("Enter the number of lines of first matrix");
+        int a = scn.nextInt();
+        System.out.println("Enter the numbers of columns of first numbers and lines of second");
+        int b = scn.nextInt();
+        System.out.println("Enter the numbers of columns of second matrix");
+        int c = scn.nextInt();
+        int MatrixA [][]= new int [a][b];
+        int MatrixB [][] = new int [b][c];
+        int MatrixAB [][] = new int [a][c];
+        System.out.println("Please, full the first matrix");
+        for(int i = 0;i<a;i++){ // вводим данные первой матрицы
+            for(int j=0;j<b;j++){
+                MatrixA[i][j]=scn.nextInt();
+            }
+        }
+        System.out.println("Please, full the first matrix");
+        for(int i =0;i<b;i++) { // вводим данные второй матрицы
+            for(int j = 0; j<c;j++){
+                MatrixB[i][j] = scn.nextInt();
+            }
+        }
+        for(int i = 0; i<a;i++){
+            for (int u = 0;u<c;u++){
+                for(int j =0;j<b;j++){
+                    MatrixAB [i][u]= MatrixA[i][j]*MatrixB[j][u];
+                }
+            }
+        }
+        for (int i =0;i<a;i++){ // вывод третьей матрицы
+            for (int j=0;j<c;j++){
+                System.out.println(MatrixAB[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
