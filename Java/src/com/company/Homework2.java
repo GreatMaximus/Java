2f0dd54a804ab30dbe89a237eb24681156ed9612
GreatMaximus <ablgl666@gmail.com>
2023-03-11 22:27:47 +0200

    homework2

--- Java/src/com/company/Homework2.java
+++ Java/src/com/company/Homework2.java
@@ -5,12 +5,26 @@ import java.util.Arrays;
 public class Homework2 {
     public static void main(String[] args) {
         String[] array = {"K","O","ZH", "Z", "G", "S", "F"};
-        String [] array1 = new String [array.length];
+        String[] array1 = new String [array.length];
 
-        for(int i = array.length-1; i >=0;i--){
-            System.out.print(array[i] + " ");
+        for(int i = array.length-1; i>=0;i--){
+            array1[array.length-1-i]=array[i];
 
         }
+        System.out.println(Arrays.toString(array1));
+
+
+
+
+
         }
-}
+
+        }
+
+        //for(int i = array.length-1; i >=0;i--){
+            //System.out.print(array[i] + " ");
+
+
+
+
 
