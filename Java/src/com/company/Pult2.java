086d2d7125c71f5e7009682b7930f09f27e31ad7
GreatMaximus <ablgl666@gmail.com>
2023-03-24 12:29:57 +0200

    Pult

--- Java/src/com/company/Pult2.java
+++ Java/src/com/company/Pult2.java
@@ -4,15 +4,58 @@ import java.util.Scanner;
 
 public class Pult2 {
     public static void main (String [] args){
-        System.out.println(TVMoving());
+        Scanner scn = new Scanner (System.in);
+        String TVMoving;
+        System.out.println("What would you like to do? TV on, TV off, Volume +, Volume -, Channel +, Channel -");
+        TVMoving = scn.nextLine();
+        switch(TVMoving){
+            case "TV on":
+                System.out.println(TVOn());
+                break;
+            case "TV off":
+                System.out.println(TVOff());
+                break;
+            case "Volume +":
+                System.out.println(VolumePlus());
+                break;
+            case "Volume -":
+                System.out.println(VolumeMinus());
+                break;
+            case "Channel +":
+                System.out.println(ChannelPlus());
+                break;
+            case "Chanel -":
+                System.out.println(ChannelMinus());
+                break;
+
+            default:
+                System.out.println("Error");
+        }
 
 
     }
-    public static String TVMoving (){
-        String TVOn;
-        Scanner scn = new Scanner (System.in);
-        System.out.println("Put 'TV on', if you want it");
-        TVOn = scn.nextLine();
+    public static String TVOn (){
+        String TVOn = "TV on";
         return TVOn;
+    }
+    public static String TVOff (){
+        String TVOff = "TV off";
+        return TVOff;
+    }
+    public static String VolumePlus (){
+        String VolumePlus = "Volume +";
+        return VolumePlus;
+    }
+    public static String VolumeMinus () {
+        String VolumeMinus = "Volume -";
+        return VolumeMinus;
+    }
+        public static String ChannelPlus (){
+            String ChannelPlus = "Channel +";
+            return ChannelPlus;
+        }
+        public static String ChannelMinus (){
+            String ChannelMinus = "Volume -";
+            return ChannelMinus;
     }
 }
